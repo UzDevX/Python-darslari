7b565376f14d544c954be9d8b9ad97735e1d31d0
Otamirzayev Abdulloh <otamirzayevabdulloh1@gmail.com>
2025-06-14 18:04:03 +0900

    +dars

--- Sonlar.py
+++ Sonlar.py
@@ -1,5 +1,27 @@
-# Mavzu: Sonlar
-# 2025.05.21
+# 2025.06.14
+# Mavzu:6-dars:Sonlar
 # Muallif: Abdulloh
 
 
+a = 20
+b = 5.5 
+temp = 36.6
+# print(tpye(a))
+aholi_soni = 7_594_579_834
+print("Aholi_soni; ", aholi_soni)
+x, y, z = 10, 6.5, -56
+c = a*b 
+d = 100//2
+radius = 20
+PI = 3.14159
+diametr = 2*radius
+print("Aylana uzunligi=", PI*diametr)
+
+ism = "Jobir"
+yosh = 36
+xabar = ism + ' ' + str(yosh) + ' yoshda'
+print(xabar)
+
+t_yil = int(input("Tig'ilgan yilinggizni kiriting: "))
+yosh = 2025 - t_yil
+print("Siz ", yosh, " yoshda ekansiz")
